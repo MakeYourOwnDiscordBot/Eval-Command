a3b78931242e4bc1d63917e4913543ce2c715304
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2021-01-10 19:09:39 +0900

    Update eval.js

--- eval/commands/eval.js
+++ eval/commands/eval.js
@@ -4,16 +4,18 @@ module.exports = {
   name: "eval",
   ownerOnly: true,
   
-  async execute(message,args) {
-		var code = args;
+  async execute(message,args,client) {
+    console.log(args)
+    let e
+		var code = message.content.slice(client.prefix.length+4).split(' ');
 		try {
-      await eval(args.join(' '));
+      e = await eval(code.join(' '));
 			message.react('✅');
 		} catch (error) {
 			message.channel.send({
 				embed: {
 					title: 'ERROR',
-					description: `コード\`\`\`javascript\n' ${code} '\`\`\`ERROR内容\`\`\`' ${error} '\`\`\``
+					description: `コード\`\`\`javascript\n ${code} \`\`\`ERROR内容\`\`\` ${error} \`\`\``
 				}
 			});
 			message.react('❎');
